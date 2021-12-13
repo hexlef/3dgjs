149e6cf4a8bb01823d4f403018022c538f8c8ddb
Marcus Aku-Leh <95942118+hexlef@users.noreply.github.com>
2021-12-13 17:55:02 +0000

    5th h

--- script.js
+++ script.js
@@ -11,6 +11,48 @@ function setup() {
 }
 }
 
+function lineline(x1,y1,x2,y2,x3,y3,x4,y4){
+  var uA = ((x4-x3)*(y1-y3) - (y4-y3)*(x1-x3)) / ((y4-y3)*(x2-x1) - (x4-x3)*(y2-y1));
+  var uB = ((x2-x1)*(y1-y3) - (y2-y1)*(x1-x3)) / ((y4-y3)*(x2-x1) - (x4-x3)*(y2-y1));
+  
+  if (uA >= 0 && uA <= 1 && uB >= 0 && uB <= 1) {
+
+    // optionally, draw a circle where the lines meet
+
+    var intersectionX = x1 + (uA * (x2-x1));
+
+    var intersectionY = y1 + (uA * (y2-y1));
+
+    //fill(255,0,0);
+
+    //noStroke();
+
+    ellipse(intersectionX,intersectionY, 20,20);
+
+    var out=[intersectionX, intersectionY];
+    return out;
+
+  }
+
+  return false;
+}
+
+class voxel{
+  constructor(x,y,z){
+    this.x=x;
+    this.y=y;
+    this.z=z;
+    this.size=50;
+  }
+
+  raycast(p1,p2){
+    let l1=lineline(p1.x, p1.y, p2.x, p2.y, this.x-(this.size/2),this.y+(this.size/2),this.x-(this.size/2),this.y-(this.size/2));
+    let l2=lineline(p1.z, p1.y, p2.z, p2.y, this.z+(this.size/2),this.y+(this.size/2),this.z+(this.size/2),this.y-(this.size/2));
+    return {x:l1[0],y:l2[1],z:l2[0]]};
+  }
+
+}
+
 function draw() {
   player.velocity.x=0;
   player.velocity.z=0;
